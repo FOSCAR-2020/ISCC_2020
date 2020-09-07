7ad74629f929e0f6bb6d1fe33ee8163fce46f877
CMS0503 <tyms0503@gmail.com>
2020-09-07 22:28:10 +0900

    Add mode color & tag

--- src/gps/rviz_visualization/src/global_map_plotter.py
+++ src/gps/rviz_visualization/src/global_map_plotter.py
@@ -28,7 +28,9 @@ MARKERS_MAX = 100
 
 path_x = []
 path_y = []
+modes = []
 path_len = 0
+current_mode = 0
 
 # with open(ROS_HOME + "/straight.txt") as f:
 for path_file in sys.argv[1].split(','):
@@ -37,10 +39,12 @@ for path_file in sys.argv[1].split(','):
       for line in f.readlines():
         x = round(float(line.strip().split()[0]),4)
         y = round(float(line.strip().split()[1]),4)
+        mode = int(line.strip().split()[2])
         path_x.append(x)
         path_y.append(y)
+        modes.append(mode)
         path_len += 1
-        #print(x, y)
+        print(x, y, mode)
 
 rospy.sleep(1)
 
@@ -57,14 +61,48 @@ while path_len > count:
   marker.color.a = 1.0
   marker.color.r = 1.0
   marker.color.g = 1.0
-  marker.color.b = 0.0
+  marker.color.b = 1.0
+
+  if modes[count]%2 == 0:
+    marker.color.g = 0.0
+
+  if modes[count]%2 == 1:
+    marker.color.b = 0.0  
+
   marker.pose.orientation.w = 1.0
   marker.pose.position.x = path_x[count]
   marker.pose.position.y = path_y[count]
   marker.pose.position.z = 0
 
-
   markerArray.markers.append(marker)
+
+  # Add mode color && tag
+  start_x = 0
+  start_y = 0
+  if modes[count] != current_mode:
+    current_mode = modes[count]
+    start_x = path_x[count]
+    start_y = path_y[count]
+
+    text_marker = Marker()
+    
+    text_marker.header.frame_id = "/base_link"
+    text_marker.type = marker.TEXT_VIEW_FACING
+    text_marker.action = marker.ADD
+    text_marker.scale.z = 1
+
+    text_marker.color.a = 1.0 
+    text_marker.color.r = 0.0
+    text_marker.color.g = 1.0
+    text_marker.color.b = 0.0
+
+    text_marker.pose.position.x = path_x[count]
+    text_marker.pose.position.y = path_y[count]
+    text_marker.pose.position.z = 1
+
+    text_marker.text="mode : " + str(modes[count])
+
+    markerArray.markers.append(text_marker)
 
   id = 0
   for m in markerArray.markers:
@@ -79,16 +117,16 @@ while path_len > count:
   rospy.sleep(0.001)
 
 # for test
-while not rospy.is_shutdown():
-  try :
-    idx = int(input("input index : "))
-    target_point = PointStamped()
-    target_point.header.frame_id = "/base_link"
-    target_point.point.x = path_x[idx]
-    target_point.point.y = path_y[idx]
+# while not rospy.is_shutdown():
+#   try :
+#     idx = int(input("input index : "))
+#     target_point = PointStamped()
+#     target_point.header.frame_id = "/base_link"
+#     target_point.point.x = path_x[idx]
+#     target_point.point.y = path_y[idx]
 
-    target_point_pub.publish(target_point)
-  except:
-    pass
-  rospy.sleep(0.5)
+#     target_point_pub.publish(target_point)
+#   except:
+#     pass
+#   rospy.sleep(0.5)
 ##
