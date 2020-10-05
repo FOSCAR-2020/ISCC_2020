296983278ce548f1093deecbdf6593befe0c679f
plmoknijb15 <plmoknijb15@naver.com>
2020-10-05 19:16:37 +0900

    for school test

---
+++ src/gps/pure_pursuit/paths/t/waypoint_translation.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+
+a = 0
+b = 0
+a_trig = False
+b_trig = False
+f_trig = False
+file_name = ''
+path = ''
+
+def parse_txt(path):
+    f = open(path, "r")
+    data = f.read()
+    f.close()
+
+    data = data.rstrip().split('\n')
+    data = [x.split(' ') for x in data]
+    #print(data)
+    data = np.array(data, np.float64)
+
+    return data
+
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+
+    if len(file_name) == 0:
+        print('The file is not exist')
+        exit(-1)
+
+    path = parse_txt(file_name)
+
+    k_city_start_position = path[0]
+    mode = k_city_start_position[2]
+
+    school_start_position = np.array([955568.875223, 1956919.86782, mode],np.float64)
+    print("k_city_start_position : {}".format(k_city_start_position))
+    print("school_start_position : {}".format(school_start_position))
+
+    # offset = np.array([19923.241491,  40945.30306, 0])
+    offset = school_start_position - k_city_start_position
+
+    print("offset : {}".format(offset))
+    
+    #### for rotation ####
+    theta = 80 # degree
+    theta = theta * np.pi / 180
+    rotation_matrix = np.array([[np.cos(theta), np.sin(theta), 0],[-np.sin(theta), np.cos(theta), 0], [0, 0, 1]], np.float64)
+    
+    rotation_path = path - k_city_start_position
+    #rotation_path = path - offset
+
+    rotation_path = np.dot(rotation_path, rotation_matrix)
+    
+    rotation_path += k_city_start_position
+    #rotation_path += offset
+    ######################
+    
+    
+    #### for parallel translation ####
+    new_path = rotation_path + offset    
+    
+    ##################################
+    
+    f = open(file_name.split('.')[0]+'.trs.txt','w')
+    out_str = ''
+    for point in new_path:
+        out_str += str(point[0]) + ' ' + str(point[1]) + ' ' + str(int(point[2])) + '\n'
+    f.write(out_str)
+    f.close()
---
+++ src/gps/rviz_visualization/src/surface_plotter_school.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('visualization_marker_tutorials')
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+import rospy
+import rospkg
+import math, sys
+
+rospy.init_node('surface_plotter')
+topic = 'visualization_marker_array2'
+publisher = rospy.Publisher(topic, MarkerArray, queue_size=100)
+rospack = rospkg.RosPack()
+markerArray = MarkerArray()
+ROS_HOME = rospack.get_path('pure_pursuit')
+
+count = 0
+MARKERS_MAX = 100
+
+path_x = []
+path_y = []
+path_len = 0
+
+# with open(ROS_HOME + "/straight.txt") as f:
+with open(ROS_HOME + "/paths/t/surface.txt") as f:
+  for line in f.readlines():
+    x = float(line.strip().split()[0])
+    y = float(line.strip().split()[1])
+    path_x.append(x)
+    path_y.append(y)
+    path_len += 1
+    #print(x, y)
+
+rospy.sleep(1)
+
+while path_len > count:
+  marker = Marker()
+  marker.header.frame_id = "/base_link"
+  marker.type = marker.SPHERE
+  marker.action = marker.ADD
+
+  marker.scale.x = 0.3
+  marker.scale.y = 0.3
+  marker.scale.z = 0.3
+
+  marker.color.a = 1.0
+  marker.color.r = 1.0
+  marker.color.g = 1.0
+  marker.color.b = 1.0
+  marker.pose.orientation.w = 1.0
+  marker.pose.position.x = path_x[count]
+  marker.pose.position.y = path_y[count]
+  marker.pose.position.z = 0
+
+
+  markerArray.markers.append(marker)
+
+  id = 0
+  for m in markerArray.markers:
+    m.id = id
+    id += 1
+
+  #print(count)
+  publisher.publish(markerArray)
+
+  count += 1
+  rospy.sleep(0.0001)
